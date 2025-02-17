4ebe7f3a9349b8fc2c49b3ea8844c5a583ea12f7
oligarc <oliver25399@gmail.com>
2025-02-17 10:56:34 +0100

    prueba

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -13,7 +13,7 @@ export function Welcome() {
               className="block w-full dark:hidden"
             />
             <img
-              src={logoDark}
+              src={logoDarkkkk}
               alt="React Router"
               className="hidden w-full dark:block"
             />
