6e97510ee56711153674031ed2cccbd0431393ea
jvillos <jvillosl14@gmail.com>
2025-02-26 13:33:23 +0100

    basic movie and movie interfaces

--- app/Types/interfaces.ts
+++ app/Types/interfaces.ts
@@ -29,3 +29,37 @@ export interface Genre{
     id:number,
     name:string
 }
+
+export interface BasicMovie{
+    id:number,  
+    title:string,
+    overview:string,
+    poster_path:string,
+    popularity:number,
+}
+
+export interface Movie {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+  
+export interface MovieResponse {
+    dates: {
+        maximum: string;
+        minimum: string;
+    };
+    page: number;
+    results: Movie[];
+}
