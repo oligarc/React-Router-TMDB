fde60f93c0a1c287a97faa3f1741960af40e86e1
jvillos <jvillosl14@gmail.com>
2025-03-09 14:42:54 +0100

    hero adjusted w-full

--- app/layouts/index.tsx
+++ app/layouts/index.tsx
@@ -13,7 +13,7 @@ function Index() {
     <>
     
     <Header backgroundcolor="bg-yellow-500" textcolor="text-black" />
-    <div className="max-w-7xl mx-auto my-5">
+    <div className="w-full mx-auto">
       <Outlet />
     </div>
     <Footer backgroundcolor="bg-amber-400" />
--- app/routes/details.tsx
+++ app/routes/details.tsx
@@ -107,99 +107,102 @@ export default function Details() {
   if (!movie) return <div>Movie not found.</div>;
 
   return (
-    <div className="container mx-auto p-4 space-y-8">
+    <>
       {/* Hero section with movie poster */}
-      <div className="relative">
+      <div className="w-full relative mb-3">
         <img
           src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`}
           alt={movie.title}
-          className="w-full h-96 object-cover rounded-lg shadow-lg"
+          className="w-full h-96 object-cover"
         />
         <div className="absolute inset-0 bg-gradient-to-t from-black opacity-50"></div>
       </div>
+      <div className="container mx-auto space-y-8">
 
-      {/* Movie details */}
-      <div className="flex flex-col md:flex-row items-start space-y-6 md:space-y-0 md:space-x-8">
-        <div className="w-full md:w-1/3">
-          <img
-            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-            alt={movie.title}
-            className="w-full h-80 object-cover rounded-lg shadow-lg"
-          />
-        </div>
 
-        <div className="flex flex-col space-y-4 w-full md:w-2/3">
-          <h1 className="text-4xl font-bold text-gray-800">{movie.title}</h1>
-          <p className="text-lg text-gray-600">{movie.overview}</p>
-          <div className="flex items-center space-x-4">
-            <p className="text-lg font-semibold text-gray-700">Rating:</p>
-            <p className="text-xl font-bold text-green-500">
-              {movie.vote_average.toFixed(1)}
-            </p>
-          </div>
-          <div className="flex items-center space-x-4">
-            <p className="text-lg font-semibold text-gray-700">Release Date:</p>
-            <p className="text-lg text-gray-600">{movie.release_date}</p>
+        {/* Movie details */}
+        <div className="flex flex-col md:flex-row items-start space-y-6 md:space-y-0 md:space-x-8 max-w-7xl mx-auto">
+          <div className="w-full md:w-1/3">
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+              className="w-full h-80 object-cover rounded-lg shadow-lg"
+            />
           </div>
 
-          {movie.genres && movie.genres.length > 0 && (
-            <div className="mt-4">
-              <h3 className="text-xl font-semibold">Genres:</h3>
-              <ul className="flex space-x-4 mt-2">
-                {movie.genres.map((genre) => (
-                  <li key={genre.id} className="bg-gray-500 px-3 py-1 rounded-full text-sm">
-                    {genre.name}
-                  </li>
-                ))}
-              </ul>
+          <div className="flex flex-col space-y-4 w-full md:w-2/3">
+            <h1 className="text-4xl font-bold text-gray-800">{movie.title}</h1>
+            <p className="text-lg text-gray-600">{movie.overview}</p>
+            <div className="flex items-center space-x-4">
+              <p className="text-lg font-semibold text-gray-700">Rating:</p>
+              <p className="text-xl font-bold text-green-500">
+                {movie.vote_average.toFixed(1)}
+              </p>
             </div>
+            <div className="flex items-center space-x-4">
+              <p className="text-lg font-semibold text-gray-700">Release Date:</p>
+              <p className="text-lg text-gray-600">{movie.release_date}</p>
+            </div>
+
+            {movie.genres && movie.genres.length > 0 && (
+              <div className="mt-4">
+                <h3 className="text-xl font-semibold">Genres:</h3>
+                <ul className="flex space-x-4 mt-2">
+                  {movie.genres.map((genre) => (
+                    <li key={genre.id} className="bg-gray-500 px-3 py-1 rounded-full text-sm">
+                      {genre.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+
+        {/* Action Buttons (e.g., Add to Favorites, Add to Watchlist) */}
+        <div className="flex justify-center mt-6">
+
+          {isFavorite ? (
+            <button
+              className="bg-red-600 text-white px-6 py-3 rounded-lg hover:bg-red-700 transition duration-300 mr-4"
+              onClick={handleRemoveFromFavorites}
+            >
+              Remove from Favorites
+            </button>
+          ) : (
+            <button
+              className="bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-red-700 transition duration-300 mr-4"
+              onClick={handleAddToFavorites}
+            >
+              Add to Favorites
+            </button>
           )}
+
+          {isInWatchlist ? (
+            <button
+              className="bg-red-600 text-white px-6 py-3 rounded-lg hover:bg-red-700 transition duration-300"
+              onClick={handleRemoveFromWatchlist}
+            >
+              Remove from Watchlist
+            </button>
+          ) : (
+            <button
+              className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition duration-300"
+              onClick={handleAddToWatchlist}
+            >
+              Add to Watchlist
+            </button>
+          )}
+
         </div>
-      </div>
 
-      {/* Action Buttons (e.g., Add to Favorites, Add to Watchlist) */}
-      <div className="flex justify-center mt-6">
-        
-      {isFavorite ? (
-          <button
-            className="bg-red-600 text-white px-6 py-3 rounded-lg hover:bg-red-700 transition duration-300 mr-4"
-            onClick={handleRemoveFromFavorites}
-          >
-            Remove from Favorites
-          </button>
-        ) : (
-          <button
-            className="bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-red-700 transition duration-300 mr-4"
-            onClick={handleAddToFavorites}
-          >
-            Add to Favorites
-          </button>
+        {/* Message */}
+        {message && (
+          <div className={`mt-4 p-4 text-white text-center rounded-b-lg ${messageType === 'success' ? 'bg-green-500' : 'bg-red-500'}`}>
+            {message}
+          </div>
         )}
-
-        {isInWatchlist ? (
-          <button
-            className="bg-red-600 text-white px-6 py-3 rounded-lg hover:bg-red-700 transition duration-300"
-            onClick={handleRemoveFromWatchlist}
-          >
-            Remove from Watchlist
-          </button>
-        ) : (
-          <button
-            className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition duration-300"
-            onClick={handleAddToWatchlist}
-          >
-            Add to Watchlist
-          </button>
-        )}
-        
       </div>
-
-      {/* Message */}
-      {message && (
-        <div className={`mt-4 p-4 text-white text-center rounded-b-lg ${messageType === 'success' ? 'bg-green-500' : 'bg-red-500'}`}>
-          {message}
-        </div>
-      )}
-    </div>
+    </>
   );
 }
